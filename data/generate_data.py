fed0be208d6dfeb539872134bfe512f78ea0183f
mbary <michal.barrington@gmail.com>
2025-07-29 12:45:00 +0200

    updates to generating synth data

--- data/generate_data.py
+++ data/generate_data.py
@@ -7,6 +7,7 @@ import itertools
 from pydantic import BaseModel, Field
 import instructor
 import asyncio
+from rich import print
 from openai import AsyncOpenAI
 from tqdm.asyncio import tqdm
 from dotenv import load_dotenv
@@ -17,10 +18,7 @@ OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 MODEL_FREE = "deepseek/deepseek-chat-v3-0324:free"
 MODEL = "deepseek/deepseek-chat-v3-0324"
 
-class Zone(BaseModel):
-    name: Literal["office", "kitchen", "living room", "bedroom", "all"] = Field(
-        description="The name of the zone where the command will be executed.")
-    
+semaphore = asyncio.Semaphore(5)
 
 class SynthCommand(BaseModel):
     full_command: str = Field(description="A command, including both wakeword phrase and an action, a user would give to the light controlling system.",
@@ -34,11 +32,16 @@ class SynthCommand(BaseModel):
     wakeword_phrase: str = Field(description="The wakeword phrase to trigger the command.",
                                  examples=["Hey Bridgette", "Heeey Bridgette!!", "Hi Bridgette", "Bridgette"],)
     
-    zone: Zone = Field(description="The zone where the command will be executed.",)
-    action: str = Field(description="The action to be performed in the specified zone.",
+    zone: Literal["office", "kitchen", "lounge","lounge floor lights", "bedroom", "all","tv"] = Field(description="The name of the zone where the command will be executed.")
+                
+    action: Literal["turn on", "turn off", "dim", "set color", "set brightness", "set scene", "set lights"] = Field(description="The action to be performed in the specified zone.",
                         examples=["turn on", "turn off", "dim", "set color", "set brightness", "set scene", "set lights"],
                         )
 
+    scenes: Optional[Literal["natural light","concenrate", "energize", "relax", "read", "dimmed", "shrexy", "soho", "disturbia"]] = Field(
+        description="The scene to be set in the specified zone. A scene can be set only on an entire zone, not on a specific light.",
+        default=None
+        )
 
 class SynthResponse(BaseModel):
     commands: List[SynthCommand] = Field(description="A list of commands that can be executed by the light controlling system.")
@@ -46,7 +49,9 @@ class SynthResponse(BaseModel):
 
 
 async def generate_data(n_queries:int = 10,
-                        model: str = MODEL_FREE) -> List[SynthCommand]:
+                        model: str = MODEL_FREE
+                        # model: str = MODEL
+                        ) -> List[SynthCommand]:
     """Generate data for the light controlling system."""
     client = instructor.from_openai(AsyncOpenAI(base_url=BASE_URL_OPENROUTER, api_key=OPENROUTER_API_KEY))
     system_prompt = f"""You are an assistant that generates realistic commands a user would give to a light controlling system.
@@ -55,41 +60,79 @@ async def generate_data(n_queries:int = 10,
     - dim
     - turn on
     - turn off
+    - set brightness
+    - set scene
 
     Available zones:
-    - kitchen
     - office
-    - living room
+    - lounge
+    - lounge floor lights
     - bedroom
     - all
 
+    Scenes per zone:
+    - office:
+      - 'phthalocyanine green love', 
+      - 'soho' 
+      - 'shrexy'
+      - 'tri colour'
+      - 'bloodbath'
+      - 'relax', 
+      - 'read'
+      - 'energize' 
+      - 'rest'
+      - 'vapor wavey'
+      - 'nightlight'
+      - 'disturbia'
+      - 'dimmed'
+      - 'concentrate'
+      - 'natural light'
+    - lounge:
+      - 'galaxy'
+      - 'starlight'
+      - 'valley dawn'
+      - 'nightlight'
+      - 'warm embrace'
+    - bedroom:
+      - 'energize'
+      - 'nightlight'
+      - 'read'
+      - 'rest'
+      - 'relax'
+      - 'concentrate'
+      - 'natural light'
+
     wakeword phrase variation:
     - The wakeword phrase **MUST** be a variation of `Hey Bridgette`
-        - You can do `Heeey Bridgette`; `Heeyy Bridgette`; `Bridgette` `Hiii Bridgette`etc
+
     - You can be creative and use any variation of the wakeword phrase you can think off
 
     commands:
-    - A command can be **ONLY** for one zone
-    - You **CANNOT** create chained commands e.g. "hey bridgette, turn on all lights and dim them to 40%"
-    
+    - A command can be **ONLY** for one zone at a time
+      - You **CANNOT** create chained commands e.g. "hey bridgette, turn on all lights and dim them to 40%"
+    - Scenes can **ONLY** be set for zones with listed scenes
+      - Zones without scenes support only on/off functionality
+
     Respond with a JSON object with the following structure:
     {SynthResponse.model_json_schema()}
     """
 
     user_prompt = f"""Generate {n_queries} diverse commands"""
+    async with semaphore:
+        commands = await client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role":"system", "content":system_prompt},
+                {"role":"user", "content":user_prompt}
+            ],
+            response_model=SynthResponse,
+            max_tokens=4096,
+            max_retries=5
+        )
 
-    commands = await client.chat.completions.create(
-        model=model,
-        messages=[
-            {"role":"system", "content":system_prompt},
-            {"role":"user", "content":user_prompt}
-        ],
-        response_model=SynthResponse
-    )
+        return commands.commands
 
-    return commands.commands
-
-async def main(n_runs:int = 10):
+async def main(n_runs:int = 100):
     """
     Run generate_data n times
     Each run generates n_runs*10 commands
@@ -105,16 +148,10 @@ async def main(n_runs:int = 10):
 results = await main()
 
 
-
-from rich import print
 print(results)
-len(results)
-
-type(results[0][0])
-results[0][0]
+a = results[0].json()
+type(a)
 
 if __name__=="__main__":
     results = asyncio.gather(main())
     print(results)
-
-    
