78e724bdf71b52646684c862372a59d56cc6c4a5
mbary <michal.barrington@gmail.com>
2025-08-06 19:49:45 +0200

    corrected the data generation and created a 4k dataset without DIM or set_color

--- prometheus_ai/data/generate_data.py
+++ prometheus_ai/data/generate_data.py
@@ -12,7 +12,9 @@ from rich import print
 from openai import AsyncOpenAI
 from tqdm.asyncio import tqdm
 
-from ..utils.project_types import Brightness
+import sys
+sys.path.append(str(Path(__file__).parent.parent))
+from utils.project_types import Brightness
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -23,12 +25,10 @@ MODEL_FREE = "deepseek/deepseek-chat-v3-0324:free"
 MODEL = "deepseek/deepseek-chat-v3-0324"
 DATA_DIR = Path("./synth_commands")
 
-semaphore = asyncio.Semaphore(5)
+semaphore = asyncio.Semaphore(10)
   
 
 class SynthCommand(BaseModel):
-    thinking: str = Field(description="Think about the action to be executed. What action does the user want to perform?")
-
     full_command: str = Field(description="A command, including both wakeword phrase and an action, a user would give to the light controlling system.",
                          examples=["Hey Bridgette, turn on the lights in the office",
                                    "Heeey Bridgette!! Turn off the lights in the lounge", 
@@ -41,24 +41,27 @@ class SynthCommand(BaseModel):
     wakeword_phrase: str = Field(description="The wakeword phrase to trigger the command.",
                                  examples=["Hey Bridgette", "Heeey Bridgette!!", "Hi Bridgette", "Bridgette"],)
     
-    action: Literal["turn_on", "turn_off", "dim", "set_color", "set_brightness", "set_scene", "set_temperature"] = Field(description="The action to be performed in the specified zone.")
+    action: Literal["turn_on", "turn_off",
+                    #  "dim", 
+                    #  "set_color", 
+                     "set_brightness", "set_scene", "set_temperature"] = Field(description="The action to be performed in the specified zone.")
     
     zone: Literal["office", "lounge","lounge floor lights", "bedroom", "all","tv"] = Field(description="The name of the zone where the command will be executed.")
 
-    light: Optional[str] = Field(
+    light: Union[str, None] = Field(
         description="The name of the light where the command will be executed", default=None,)                
 
-    scene: Optional[Literal['natural light', 'relax, ', 'bloodbath', 'rest', 'disturbia', 'relax', 'energize ', 
+    scene: Union[Literal['natural light', 'relax, ', 'bloodbath', 'rest', 'disturbia', 'relax', 'energize ', 
                              'concentrate', 'read', 'warm embrace', 'galaxy', 'phthalocyanine green love', 'starlight',
-                             'tri colour', 'shrexy', 'nightlight', 'energize', 'vapor wavey', 'dimmed', 'valley dawn', 'soho ']] = Field(description="The scene to be set in the specified zone. A scene can be set only on an entire zone, not on a specific light.",
+                             'tri colour', 'shrexy', 'nightlight', 'energize', 'vapor wavey', 'dimmed', 'valley dawn', 'soho '], None] = Field(description="The scene to be set in the specified zone. A scene can be set only on an entire zone, not on a specific light.",
                                                                                                                                          default=None)
-    temperature: Optional[int] = Field(description="The warmth of the light",
+    temperature: Union[int, None] = Field(description="The warmth of the light",
                                        ge=153, le=500,
                                        examples=[153, 200, 300, 400, 500],
                                        default=None)
-    brightness: Optional[Brightness] = Field(default=None,)
+    brightness: Union[Brightness, None] = Field(default=None,)
 
-    color: Optional[str] = Field(description="The color to be set in the specified zone.",
+    color: Union[str, None] = Field(description="The color to be set in the specified zone.",
                                  examples=["red", "blue", "green", "yellow", "purple", "orange", "pink", "white"],
                                  default=None)
 
@@ -73,13 +76,10 @@ class Scenario(BaseModel):
     wakeword_phrase: str
     action_type: str
     zone: str
-    scene: Optional[str]
-    light: Optional[str]
-    temperature: Optional[int]
-    brightness: Optional[Union[int,float]]
-    brightness_relative: Optional[bool]
-    brightness_up_down: Optional[str]
-    color: Optional[str]
+    scene: Union[str, None]
+    light: Union[str, None]
+    temperature: Union[int, None]
+    brightness: Union[Brightness, None]
     split: Literal["train", "test"]
 
 
@@ -91,35 +91,31 @@ async def generate_commands(n_queries:int = 10,
     client = instructor.from_openai(AsyncOpenAI(base_url=BASE_URL_OPENROUTER, api_key=OPENROUTER_API_KEY))
     system_prompt = f"""You are an assistant that generates realistic commands a user would give to a light controlling system.
 
-    Commands per device:
+    #Commands per device:
     - Light:
       - turn_on
       - turn_off
-      - dim
       - set_brightness
-      - set_color
       - set_temperature
     - Plug:
       - turn_on
       - turn_off
     
-    Available commands:
-    - dim
+    #Available commands:
     - turn_on
     - turn_off
     - set_brightness
     - set_scene
     - set_temperature
-    - set_color
     
-    Available zones:
+    #Available zones:
     - office
     - lounge
     - lounge floor lights
     - bedroom
     - all
 
-    Scenes per zone:
+    #Scenes per zone:
     - office:
       - 'phthalocyanine green love', 
       - 'soho' 
@@ -151,7 +147,7 @@ async def generate_commands(n_queries:int = 10,
       - 'concentrate'
       - 'natural light'
     
-    Lights per zone:
+    #Lights per zone:
     - office:
       - 'desk' (light)
       - 'ceiling' (light)
@@ -170,12 +166,12 @@ async def generate_commands(n_queries:int = 10,
     - tv:
       - 'sub' (plug)
 
-    wakeword phrase variation:
+    #Wakeword phrase variation:
     - The wakeword phrase **MUST** be a variation of `Hey Bridgette`
-
     - You can be creative and use any variation of the wakeword phrase you can think off
+    - The commands should vary from simple to complex, but should not exceed 20 words in total.
 
-    commands:
+    #Command creation rules:
     - A command can be **ONLY** for one zone at a time
       - You **CANNOT** create chained commands e.g. "hey bridgette, turn on all lights and dim them to 40%"
     - Scenes can **ONLY** be set for zones with listed scenes
@@ -193,8 +189,8 @@ async def generate_commands(n_queries:int = 10,
             {"role":"system", "content":system_prompt},
             {"role":"user", "content":user_prompt}
         ],
-        response_model=SynthResponse,
-        max_tokens=4096,
+        response_model=Union[SynthResponse],
+        max_tokens=8000,
         max_retries=5
     )
 
@@ -204,7 +200,7 @@ async def generate_all_commands():
     async with semaphore:
         return await generate_commands()
     
-async def main(n_runs:int = 10):
+async def main(n_runs:int = 500):
     
     tasks = [generate_all_commands() for _ in range(n_runs)]
     results = await tqdm.gather(*tasks)
@@ -212,14 +208,16 @@ async def main(n_runs:int = 10):
     print(len(results))
     return results
 
-results = await main(n_runs=300)
+results = await main(n_runs=400)
+
+print(results)
 # len(results)
 # results[0].model_dump_json
 serialised_results=[res.model_dump_json() for res in results]
 
 list_scenarios=[]
 id=0
-with open(DATA_DIR/"synth_commands_3k.jsonl", "a", encoding="utf-8") as file:
+with open(DATA_DIR/"synth_commands_4k.jsonl", "a", encoding="utf-8") as file:
   for res in results:
         file.write(
             Scenario(
@@ -231,9 +229,9 @@ with open(DATA_DIR/"synth_commands_3k.jsonl", "a", encoding="utf-8") as file:
                 light=res.light,
                 scene=res.scene,
                 temperature=res.temperature,
-                brightness=res.brightness.brightness if res.brightness else None,
-                brightness_relative=res.brightness.relative if res.brightness else None,
-                brightness_up_down=res.brightness.up_down if res.brightness else None,
+                brightness=res.brightness,#.brightness if res.brightness else None,
+                # brightness_relative=res.brightness.relative if res.brightness else None,
+                # brightness_up_down=res.brightness.up_down if res.brightness else None,
                 color=res.color,
                 split="train" if id < 2500 else "test"
             ).model_dump_json() + "\n"
@@ -248,11 +246,11 @@ with open(DATA_DIR/"synth_commands_3k.jsonl", "a", encoding="utf-8") as file:
                 light=res.light,
                 scene=res.scene,
                 temperature=res.temperature,
-                brightness=res.brightness.brightness if res.brightness else None,
-                brightness_relative=res.brightness.relative if res.brightness else None,
-                brightness_up_down=res.brightness.up_down if res.brightness else None,
+                brightness=res.brightness,#.brightness if res.brightness else None,
+                # brightness_relative=res.brightness.relative if res.brightness else None,
+                # brightness_up_down=res.brightness.up_down if res.brightness else None,
                 color=res.color,
-                split="train" if id < 2500 else "test"
+                split="train" if id < 3200 else "test"
             ).model_dump_json()
         )
         id += 1
@@ -264,8 +262,8 @@ from datasets import Dataset
 hf_ds_train = Dataset.from_list(train_scenarios)
 hf_ds_test = Dataset.from_list(test_scenarios)
 
-# hf_ds_train.push_to_hub("mbary/hue_commands_synth_3k", private=True, split="train")
-# hf_ds_test.push_to_hub("mbary/hue_commands_synth_3k", split="test")
+# hf_ds_train.push_to_hub("mbary/hue_commands_synth_4k", private=True, split="train")
+# hf_ds_test.push_to_hub("mbary/hue_commands_synth_4k", split="test")
 
 # with open(DATA_DIR/"synth_commands3_3k.jsonl","w",encoding="utf-8") as file:
 #     file.write(json.dumps(serialised_results, indent=4))
